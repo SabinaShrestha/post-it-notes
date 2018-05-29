47ac0b715864ca24a402cca7a9dc7eb212033ff9
Sabina Shrestha <sabina.pari@yahoo.com>
2018-05-29 13:24:21 -0600

    add controller and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'notes/index'
+  get 'notes/new'
+  get 'notes/show'
+  get 'notes/edit'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
